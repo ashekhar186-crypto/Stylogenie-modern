04114ed766ce9550d3b09733c39d140d36e85ee7
ashekhar186-crypto <a.shekhar186@gamil.com>
2026-04-17 18:53:54 +0530

    Clean repo artifacts and local-only files

--- backend/dist/health.test.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const vitest_1 = require("vitest");
-(0, vitest_1.describe)('sanity', () => { (0, vitest_1.it)('works', () => { (0, vitest_1.expect)(2 + 2).toBe(4); }); });
